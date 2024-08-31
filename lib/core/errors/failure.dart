5fd6c5c889738be68837517af6c67ac671b5e65a
Ahmed Zein <120061135+ahmedzein74@users.noreply.github.com>
2024-08-31 17:37:22 +0300

    create home repo impl and api service file

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -4,4 +4,5 @@ class ServerFailure extends Failure {}
 
 
 
-///I created types that inherit from it whenever I want, and at the same time, it doesn't cause issues when I pass it to the Either class.
+/*I created types that inherit from it whenever I want, and at the same time,
+ it doesn't cause issues when I pass it to the Either class.*/
--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -3,6 +3,8 @@ import 'package:bookly_app/features/home/data/models/book_model/book_model.dart'
 import 'package:dartz/dartz.dart';
 
 //repostiry pattern ====>> create all method in this feature this is useful for team work
+
+//either from dartz package
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
